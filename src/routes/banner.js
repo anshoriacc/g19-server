b141c25e05e0a52fba3564a322bc8de544cfe8a5
achmad anshori <utd.rik@gmail.com>
2023-07-16 00:31:07 +0700

    update: fix upload banner

--- src/routes/banner.js
+++ src/routes/banner.js
@@ -3,13 +3,19 @@ const router = express.Router();
 
 const { bannerController } = require('../controllers');
 
-const { verifyJWT, authorize, cloudinaryUpload } = require('../middlewares');
+const {
+  verifyJWT,
+  upload,
+  authorize,
+  cloudinaryUpload,
+} = require('../middlewares');
 
 router.get('/', verifyJWT, authorize.admin, bannerController.getList);
 router.post(
   '/',
   verifyJWT,
   authorize.admin,
+  upload.single,
   cloudinaryUpload('banners', true),
   bannerController.create
 );
